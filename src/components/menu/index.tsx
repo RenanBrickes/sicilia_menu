8df8d14e577ac700f9efeece12b403e5789a9f88
Renan Brickes <rbrickes@paschoalotto.com.br>
2022-07-13 10:42:12 -0300

    feat(menu): linkando menus as telas

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,23 +1,25 @@
 import { Grid } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import * as Styled from "./styled";
 
 export const MenuContainer = () => {
+  const navigate = useNavigate();
   return (
     <Styled.Container>
       <Styled.ContainerMenu>
         <Grid container spacing={3}>
           <Styled.GridMenu item xs={4}>
-            <Styled.IconDiv>
+            <Styled.IconDiv onClick={() => navigate("/")}>
               <Styled.Icone>restaurant_menu</Styled.Icone>
             </Styled.IconDiv>
           </Styled.GridMenu>
           <Styled.GridMenu item xs={4}>
-            <Styled.IconDiv>
+            <Styled.IconDiv onClick={() => navigate("/Order")}>
               <Styled.Icone>menu</Styled.Icone>
             </Styled.IconDiv>
           </Styled.GridMenu>
           <Styled.GridMenu item xs={4}>
-            <Styled.IconDiv>
+            <Styled.IconDiv onClick={() => navigate("/Help")}>
               <Styled.Icone>question_mark</Styled.Icone>
             </Styled.IconDiv>
           </Styled.GridMenu>
