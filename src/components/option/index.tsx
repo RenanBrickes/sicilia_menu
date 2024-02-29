63783eece1e6b2b422c46d174e691d52e0f5ae17
Renan Brickes <renan.brickes@fatec.sp.gov.br>
2024-02-29 11:50:08 -0300

    refactor: option

--- src/components/option/index.tsx
+++ src/components/option/index.tsx
@@ -1,15 +1,15 @@
 import { CardMedia } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import * as Styled from "./styled";
+import { ContentStyled, OptionStyled, TitleStyled } from "./styled";
 
 export const Option = ({ src, title }: { src: string; title: string }) => {
   let navigate = useNavigate();
   return (
-    <Styled.Option onClick={() => navigate(`/${title}`)}>
+    <OptionStyled onClick={() => navigate(`/${title}`)}>
       <CardMedia component="img" height="140" width="240" image={src} />
-      <Styled.Content>
-        <Styled.Title>{title}</Styled.Title>
-      </Styled.Content>
-    </Styled.Option>
+      <ContentStyled>
+        <TitleStyled>{title}</TitleStyled>
+      </ContentStyled>
+    </OptionStyled>
   );
 };
--- src/components/option/styled.tsx
+++ src/components/option/styled.tsx
@@ -1,7 +1,7 @@
 import styled, { css } from "styled-components";
 import { Card, CardContent, Typography } from "@mui/material";
 
-export const Option = styled(Card)`
+export const OptionStyled = styled(Card)`
   ${({ theme }) => css`
     background-color: ${theme.colors.blue} !important;
     max-width: 345px;
@@ -13,11 +13,11 @@ export const Option = styled(Card)`
   `}
 `;
 
-export const Content = styled(CardContent)`
+export const ContentStyled = styled(CardContent)`
   margin-top: 50px;
 `;
 
-export const Title = styled(Typography)`
+export const TitleStyled = styled(Typography)`
   ${({ theme }) => css`
     font-family: ${theme.font.family.text} !important;
     font-size: 20px !important;
